6c357fb2208f76b10a7085c721d0573ebbd316c8
gauravsh0812 <>
2022-07-06 16:06:16 -0700

    update

--- preprocessing/preprocess_dataloader.py
+++ preprocessing/preprocess_dataloader.py
@@ -9,8 +9,8 @@ import os
 from sklearn.model_selection import train_test_split
 from torch.utils.data import Dataset, DataLoader, DistributedSampler
 from collections import Counter
-from torchtext.legacy.vocab import Vocab
-# from torchtext.vocab import Vocab
+#from torchtext.legacy.vocab import Vocab
+from torchtext.vocab import Vocab
 from torch.nn.utils.rnn import pad_sequence
 from functools import partial
 
@@ -28,7 +28,7 @@ class Img2MML_dataset(Dataset):
         eqn = self.dataframe.iloc[index, 1]
         indexed_eqn = []
         for token in eqn.split():
-            if token in self.vocab.keys():
+            if vocab[token] != None:
                 indexed_eqn.append(self.vocab[token])
             else:
                 indexed_eqn.append(self.vocab['<unk>'])
